588afa78dbe2d5e34593bf20020306aff775aca2
Erik Huang <erikhuangdrums@gmail.com>
2021-02-26 18:05:53 -0800

    commit tree 9c74f97964d7c0ddbbb80044295134db1d609814

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str => {
+    const words = str.split(' ')
+    return words.map(w => w[0].toUpperCase() + w.slice(1)).join(' ')
+  })
 }
